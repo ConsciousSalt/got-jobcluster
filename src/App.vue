5935c8fdbcdf728e62fcffaa4113e9561769497a
alex <conscioussalt@gmail.com>
2023-11-23 10:31:05 +0100

    add main menu

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,12 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import MainMenu from '@/components/MainMenu.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <MainMenu/>
 </template>
 
-<style scoped>
+<style lang="scss">
 .logo {
   height: 6em;
   padding: 1.5em;
@@ -26,5 +18,14 @@ import HelloWorld from './components/HelloWorld.vue'
 }
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
+}
+
+
+body {
+  background-image: url("@/assets/Download-1920x1080-wallpaper-dragon-and-night-king-artwork-.jpg");
+  background-origin: content-box;
+  background-size: cover;
+  background-repeat: no-repeat;
+  filter: blur(4px);
 }
 </style>
---
+++ src/components/MainMenu.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <button>
+    <span class="material-symbols-outlined">menu</span>
+    </button>
+
+    <div>
+      <button>
+        <span class="material-symbols-outlined">foundation</span>
+      </button>
+      <button>
+        <span class="material-symbols-outlined">taunt</span>
+      </button>
+      <button>
+        <span class="material-symbols-outlined">tooltip</span>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import {defineComponent} from "vue";
+
+  export default defineComponent({
+    name: "Main Menu",
+    setup() {
+
+    }
+  })
+</script>
+
+<style scoped lang="scss">
+
+</style>
