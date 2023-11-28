6b89ec4786be307f7b2e60476aa016ce51794e44
alex <conscioussalt@gmail.com>
2023-11-28 08:49:25 +0100

    update App component template: add main sidebar, secondary sidebar and main content wrapper

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,125 @@
-<script setup lang="ts">
-import MainMenu from '@/components/MainMenu.vue'
-</script>
-
 <template>
-  <MainMenu/>
+  <div class="screen">
+    <div class="sidebar">
+      <div class="wrapper">
+        <MainMenu/>
+        <Dashboard>
+          <component :is="currentComponent"/>
+        </Dashboard>
+      </div>
+    </div>
+    <div class="main">
+      <MainContentPanel>
+        <HouseMemebers v-if="localStale.activePage===PAGES.houses && !!localStale.houseMembersSlug" :house-slug="localStale.houseMembersSlug"/>
+      </MainContentPanel>
+    </div>
+  </div>
+
 </template>
 
+<script lang="ts">
+import MainMenu from '@/components/MainMenu.vue'
+import Dashboard from "@/components/Dashboard.vue";
+import MainContentPanel from "@/components/MainContentPanel.vue";
+
+import HousesList from "@/components/HousesList.vue";
+import PersonsLIst from "@/components/PersonsLIst.vue";
+import QuotesList from "@/components/QuotesList.vue";
+
+import HouseMemebers from "@/components/HouseMemebers.vue";
+
+import {computed, defineComponent, provide, reactive} from "vue";
+import {PAGES} from "@/types.ts";
+
+
+export default defineComponent({
+  name: "App",
+  computed: {
+    PAGES() {
+      return PAGES
+    }
+  },
+  components: {
+    MainMenu,
+    Dashboard,
+    MainContentPanel,
+
+    HousesList,
+    PersonsLIst,
+    QuotesList,
+
+    HouseMemebers,
+  },
+  setup(){
+    const localStale = reactive({
+      activePage: PAGES.houses,
+      houseMembersSlug: '',
+    });
+
+    const setPage = (page: PAGES )=> {
+      localStale.activePage = page;
+    };
+
+    const showHouseInfo = (slug: string) => {
+      console.log("App --> showHouseInfo", slug);
+      localStale.houseMembersSlug = slug;
+    }
+
+    const currentComponent = computed<any>(() => {
+      switch (localStale.activePage) {
+        case PAGES.houses: return "HousesList";
+        case PAGES.persons: return "PersonsLIst";
+        case PAGES.quotes: return "QuotesList";
+        default: return null;
+      }
+    });
+
+
+    provide('setPage', setPage);
+    provide('showHouseInfo', showHouseInfo);
+
+    return {
+      localStale,
+      currentComponent
+    }
+  }
+})
+</script>
+
 <style lang="scss">
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+#app {
+  margin: 0;
+  padding: 0;
+  height: 100vh;
+  width: 100%;
+  overflow: hidden;
 }
 
+.screen {
+  position: static;
+  display: flex;
+  height: 100%;
+  background-color: #111827;
+  .sidebar{
+    flex-shrink: 0!important;
+    border-right-width: 1px!important;
+    border-right-style: solid;
+    width: auto!important;
+    height: auto!important;
+    position: static!important;
+    z-index: 1!important;
+    .wrapper {
+      display: flex;
+      height: 100%;
+    }
+  }
 
-body {
-  background-image: url("@/assets/Download-1920x1080-wallpaper-dragon-and-night-king-artwork-.jpg");
-  background-origin: content-box;
-  background-size: cover;
-  background-repeat: no-repeat;
-  filter: blur(4px);
+  .main{
+    display: flex;
+    flex-direction: column;
+    flex: 1 1 auto;
+    min-height: 100vh;
+    position: relative;
+  }
 }
 </style>
