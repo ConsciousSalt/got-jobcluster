6c17ccc65c6949f5ea86bdf39b52b6c3cbda63df
alex <conscioussalt@gmail.com>
2023-11-28 08:50:06 +0100

    add component for showing house members in main content component

---
+++ src/components/HouseMemebers.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="item in list">
+        <button @click="itemClickHandler(item.slug)">
+          {{item.name}}
+        </button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent, ref, watch, inject} from "vue";
+import {getHouseDetails} from "@/api.ts";
+import {ItemInfo, PAGES} from "@/types.ts";
+
+export default defineComponent({
+  name: "House members",
+  props:['houseSlug'],
+  setup(props){
+    const setPage = inject<(page: PAGES)=>void>('setPage');
+
+    const list = ref<ItemInfo[]>([]);
+    const isLoading = ref(false);
+    const itemClickHandler = (slug: string) => {
+      console.log("itemClickHandler --> ", slug);
+
+
+      setPage?.(PAGES.persons);
+    }
+
+    const loadHouseMembers = async () => {
+      isLoading.value = true;
+      list.value = await getHouseDetails(props["houseSlug"]);
+      isLoading.value = false;
+    }
+
+    watch(()=>props['houseSlug'], loadHouseMembers, {immediate: true});
+
+    return {
+      list,
+      itemClickHandler
+    }
+  }
+})
+</script>
+
+
+<style scoped lang="scss">
+
+</style>
