ead084dbba231c971a338ce3608ddd3177e54872
alex <conscioussalt@gmail.com>
2023-12-01 11:08:48 +0100

    add methods for getting personal details and random quotes

--- src/api.ts
+++ src/api.ts
@@ -1,11 +1,10 @@
 import get from "axios";
 import type {HouseListResponseType} from "@/types.ts";
-import {ItemInfo, PersonsResponse} from "@/types.ts";
+import {ItemInfo, PersonDetailedInfo, PersonsResponse, Quote} from "@/types.ts";
 
 export async function getHousesList(): Promise<HouseListResponseType[]> {
   try {
     const response = await get('https://api.gameofthronesquotes.xyz/v1/houses');
-    console.log("response --> ", response);
     if (response.status === 200) {
       return response.data;
     } else {
@@ -40,6 +39,50 @@ export async function getAllCharacters(): Promise<PersonsResponse[]> {
       return [];
     }
   } catch (e) {
+    return [];
+  }
+}
+
+export async function getCharacterDetails(slug: string): Promise<PersonsResponse|{}> {
+  try {
+    const response = await get(`https://api.gameofthronesquotes.xyz/v1/character/${slug}`);
+
+    if (response.status === 200) {
+      return response.data.find((el: any) => el.slug === slug) || {};
+    } else {
+      return {};
+    }
+  } catch (error) {
+    return {};
+  }
+}
+
+export async function getFullCharactersInfo(): Promise<PersonDetailedInfo[]> {
+  try {
+
+    const response = await get('https://thronesapi.com/api/v2/Characters');
+
+    if (response.status === 200) {
+      return response.data || [];
+    } else {
+      return [];
+    }
+  } catch (error) {
+    return [];
+  }
+}
+
+export async function getRandomQuotes(count: number): Promise<Quote[]> {
+  try {
+
+    const response = await get(`https://api.gameofthronesquotes.xyz/v1/random/${count}`);
+
+    if (response.status === 200) {
+      return response.data || [];
+    } else {
+      return [];
+    }
+  } catch (error) {
     return [];
   }
 }
