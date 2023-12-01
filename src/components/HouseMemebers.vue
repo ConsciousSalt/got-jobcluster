12aa5056d30b4184c1cc5f6c95929bcc0e864170
alex <conscioussalt@gmail.com>
2023-12-01 11:11:53 +0100

    update house memebers template styling

--- src/components/HouseMemebers.vue
+++ src/components/HouseMemebers.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
-    <ul>
-      <li v-for="item in list">
+  <div class="list-wrapper">
+    <ul class="list" :class="{'list-single-item': list.length===1}">
+      <li class="item" v-for="item in list">
         <button @click="itemClickHandler(item.slug)">
           {{item.name}}
         </button>
@@ -47,5 +47,36 @@ export default defineComponent({
 
 
 <style scoped lang="scss">
+  .list {
+    height: 100%;
+    list-style-type: none;
+    padding: 0;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: auto;
+    justify-items: center;
+    align-items: center;
+    justify-content: space-evenly;
+    align-content: space-evenly;
+    row-gap: 0.5rem;
 
+    &-single-item {
+      grid-template-columns: 1fr;
+    }
+
+    .item{
+      width: 100%;
+      height: 100%;
+      padding: 0.1rem;
+
+      button {
+        width: 75%;
+        height: 100%;
+      }
+    }
+
+    &-wrapper {
+      height: 100%
+    }
+  }
 </style>
